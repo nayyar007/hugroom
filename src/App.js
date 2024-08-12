a2f6e2255b108faca161b24c9e7956b3b0f717bc
Naveen <naveen@livelike.com>
2024-08-12 16:30:01 +0530

    updated tf1 package

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import React from 'react';
-import './App.css';
-import { LLHugMainComponent } from '@livelike/tf1-components';
-import { useLiveLikeInit } from './useLivelike.ts';
-
+import React from "react";
+import "./App.css";
+import { LLHugMainComponent } from "@livelike/tf1-components";
+import { useLiveLikeInit } from "./useLivelike.ts";
 
 function App() {
-  const clientId = 'jpyNYfoE7c1HLLa7gCydApSlh6k1vigaNbne7Key';
+  const clientId = "jpyNYfoE7c1HLLa7gCydApSlh6k1vigaNbne7Key";
   const { isLoading, userProfile } = useLiveLikeInit({
-    clientId: clientId
-  })
+    clientId: clientId,
+  });
 
   if (isLoading) {
-    return (
-      <p>Loading...</p>
-    );
+    return <p>Loading...</p>;
   }
 
   if (!userProfile) {
-    return <p>Error In Loading livelike...</p>
+    return <p>Error In Loading livelike...</p>;
   }
 
   return (
     <div className="App">
-      <LLHugMainComponent clientId={clientId}
-        customId={'tf1liv2'}
-        LiveLike={(window).LiveLike}
-        version={2}
+      <LLHugMainComponent
+        clientId={clientId}
+        customId={"tf1live"}
+        LiveLike={window.LiveLike}
+        version={3}
         returnBtnClickHandler={() => {
-        console.log("Return handled")
-      }}></LLHugMainComponent>
+          console.log("Return handled");
+        }}
+      ></LLHugMainComponent>
     </div>
   );
 }
