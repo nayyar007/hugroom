a61701c4e6fb8606c0546aa08b810a7724feece6
Naveen <naveen@livelike.com>
2024-11-14 00:21:38 +0530

    added event listener on app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 // import { LLHugMainComponent } from "@livelike/tf1-components";
 import { useLiveLikeInit } from "./useLivelike.ts";
@@ -8,6 +8,20 @@ function App() {
   const { isLoading, userProfile } = useLiveLikeInit({
     clientId: clientId,
   });
+
+  useEffect(() => {
+    const handleMessage = (event) => {
+      if (event.data.type === "success") {
+        console.log("Iframe content changed:", event.data.message);
+      }
+    };
+
+    window.addEventListener("message", handleMessage);
+
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
+  }, []);
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -22,6 +36,7 @@ function App() {
       <iframe
         src="https://tgl-dev.livelikeapp.com/"
         className="tgl-iframe"
+        title="TGL"
       ></iframe>
     </div>
   );
