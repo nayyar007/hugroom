775bfc7e14a016a499a69a3306d17f8979881bf0
Naveen <naveen@livelike.com>
2026-07-13 01:06:50 +0530

    updated hugroom content to wta package widget

--- src/App.js
+++ src/App.js
@@ -1,44 +1,55 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import { useLiveLikeInit } from "./useLivelike.ts";
-// import QRComponent from "./QRComponent";
-function App() {
-  const clientId = "q4p0w4FGx2gf0uGnqB6lB4bhrQcagfioJDsNDgs8";
-  const { isLoading, userProfile } = useLiveLikeInit({
-    clientId: clientId,
-  });
+import * as LiveLike from "@livelike/widget-elements";
+import { useEffect, useState } from "react";
+
+export default function WtaPage() {
+  const [loadWidgets, setLoadWidgets] = useState(false);
+
+  const loadLiveLikeSdk = async () => {
+    await LiveLike.LiveLikeInit({
+      clientId: "9MxRnhmq0Wkv89ESZciLBNY8ttjKce3VkSj8Te4G",
+    });
+  };
 
   useEffect(() => {
-    const handleMessage = (event) => {
-      if (event.data.type === "success") {
-        console.log("Iframe content changed:", event.data.message);
-      }
-    };
-
-    window.addEventListener("message", handleMessage);
-
-    return () => {
-      window.removeEventListener("message", handleMessage);
-    };
+    loadLiveLikeSdk().then(() => {
+      setLoadWidgets(true);
+    });
   }, []);
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
+  return loadWidgets ? (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: "10px",
+        gap: "15px",
+      }}
+    >
+      {/* text-poll */}
+      <text-poll widgetid="fcc2f850-3e82-4579-83cd-154311868879"></text-poll>
+      <text-poll widgetid="c45fb515-c26e-4571-8b44-b86826af1845"></text-poll>
 
-  if (!userProfile) {
-    return <p>Error In Loading livelike...</p>;
-  }
+      {/* image-poll */}
+      <image-poll widgetid="6455697a-4653-44a0-a3b3-64f8f832bb3a"></image-poll>
+      <image-poll widgetid="19a68d03-d864-4943-888d-59f46341aaee"></image-poll>
 
-  return (
-    <div className="App">
-      <iframe
-        title="LiveLike Content"
-        className="tgl-iframe"
-        src="https://tgl-qa.livelikeapp.com/perfect-season.html?accessToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImIzNjk4NWE2LTdkN2ItNGIzMy1hMjc3LTAwMjY5ZjdmNmZhNSIsImNsaWVudF9pZCI6Ik45dDRYd3lNMVZKRmtOV2tJZUZxcWNTSWNScDRkYjFNRVVhbmZ3R0EiLCJhY2Nlc3NfdG9rZW4iOiJjM2FkYjFlZjhkMDNjYWM3ODcwOGQ3MjdjZjg1NjNhNWQ0YmM1NjUyIiwiaXNzIjoiYmxhc3RydCIsImlhdCI6MTc2MjUyNTAwNn0.rGXfpBYeR9m_DKs6DQZnVV6l9jsH6dUfNheFaPAct90"
-      />
+      {/* text-prediction */}
+      <text-prediction widgetid="4c713672-f7bb-45a6-81ec-8a6a5deca1eb"></text-prediction>
+      <text-prediction widgetid="fdab59dd-07a5-4633-a55e-29b609511e2b"></text-prediction>
+
+      {/* image-prediction */}
+      <image-prediction widgetid="6edb3cd5-9ae4-42e2-b719-b4fb18039068"></image-prediction>
+      <image-prediction widgetid="d38cf48f-a8a5-40e0-853b-ad0801a0f572"></image-prediction>
+
+      {/* text-quiz */}
+      <text-quiz widgetid="6b898a94-90a1-40cb-92c5-73fc85a317e7"></text-quiz>
+      <text-quiz widgetid="9d6c7f6c-e2e8-47c0-b109-1d251cb35f5c"></text-quiz>
+
+      {/* image-quiz */}
+      <image-quiz widgetid="06110e01-e913-46c4-9f0c-b7b4a07d0392"></image-quiz>
+      <image-quiz widgetid="b5a9cf26-1b2d-4b70-876b-ec6f74986b65"></image-quiz>
     </div>
+  ) : (
+    <p>Loading...</p>
   );
 }
-
-export default App;
